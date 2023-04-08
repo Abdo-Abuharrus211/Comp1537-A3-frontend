200294c096b02ff08861c40764347ef13522763b
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-04-08 11:05:25 -0700

    modify index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 
 const setup = () => {
+
+
     $('#nameSearchButton').click(async () => {
         //todo send ajax to backend/server 
         const query = {
-            type : "nameSearch",
+            type: "nameSearch",
             name: $('#nameSearchInput').val(),
             projectionFilters: {
                 name: true,
@@ -11,12 +13,36 @@ const setup = () => {
             }
         }
 
-       const res = await axios.post('https://fantastic-cyan-dress.cyclic.app//search', query)
+        const res = await axios.post('https://fantastic-cyan-dress.cyclic.app//search', query)
 
         $("#searchResults").empty();
         $("#searchResults").html(JSON.stringify(res.data));
 
     });
+
+    $('#weightSearchCheckbox').click(async () => {
+        if ($("weightSearchCheckbox").is(':checked')) {
+            const query = {
+                type: "weightSearch",
+                minWeight: $('#weightLowerLimitInput').val(),
+                maxWeight: $('#weightUpperLimitInput').val(),
+                projectionFilters: {
+                    name: true,
+                    weight: true,
+
+                }
+            }
+        }
+
+        const res = await axios.post('https://fantastic-cyan-dress.cyclic.app//search', query)
+
+
+
+    });
+
+    
+
+
 }
 
 $(document).ready(setup);
@@ -59,7 +85,7 @@ $(document).ready(setup);
 // console.log('setup');
 // const setup = () => {
 //     $('#nameSearchButton').click(async () => {
-//         //todo send ajax to backend/server 
+//         //todo send ajax to backend/server
 //         const query = {
 //             type: "nameSearch",
 //             name: $('#nameSearchInput').val(),
