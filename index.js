1fcb5caf473d2c0a637732b91daa46867d0a60b1
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-04-08 09:56:33 -0700

    add other projections

--- index.js
+++ index.js
@@ -3,19 +3,45 @@ const setup = () => {
     $('#nameSearchButton').click(async () => {
         //todo send ajax to backend/server 
         const query = {
-            type : "nameSearch",
+            type: "nameSearch",
             name: $('#nameSearchInput').val(),
+            loves: $('input[name="food"]:checked').map(function () {
+                return $(this).val();
+            }).get(),
+            minWeight: $('#minWeightInput').val(),
+            maxWeight: $('#maxWeightInput').val(),
             projectionFilters: {
                 name: true,
                 weight: false,
+                loves: false,
             }
         }
 
-       const res = await axios.post('https://fantastic-cyan-dress.cyclic.app//search', query)
+        // Check if weight search is selected
+        if ($('#weightSearchCheckbox').is(':checked')) {
+            query.type = "weightSearch";
+            query.selectionArgument = { weight: { $gte: query.minWeight, $lte: query.maxWeight } };
+            query.projectionFilters = {
+                name: false,
+                weight: true,
+                loves: false,
+            }
+        }
+
+        const res = await axios.post('https://fantastic-cyan-dress.cyclic.app//search', query)
 
         $("#searchResults").empty();
         $("#searchResults").html(JSON.stringify(res.data));
+    });
 
+    $('#weightSearchCheckbox').click(function () {
+        if ($(this).is(':checked')) {
+            $('#minWeightInput').prop('disabled', false);
+            $('#maxWeightInput').prop('disabled', false);
+        } else {
+            $('#minWeightInput').prop('disabled', true);
+            $('#maxWeightInput').prop('disabled', true);
+        }
     });
 }
 
