740c082d737e7011ec08852fc25fc6c4de948fa7
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-04-08 11:49:26 -0700

    Add food search

--- index.js
+++ index.js
@@ -22,21 +22,69 @@ const setup = () => {
 
     $('#weightSearchButton').click(async () => {
         // if ($("weightSearchCheckbox").is(':checked')) {}
+        const query = {
+            type: "weightSearch",
+            minWeight: $('#weightLowerLimitInput').val(),
+            maxWeight: $('#weightUpperLimitInput').val(),
+            projectionFilters: {
+                name: true,
+                weight: true,
+            }
+
+        }
+        const res = await axios.post('https://fantastic-cyan-dress.cyclic.app//search', query)
+
+        $("#searchResults").empty();
+        $("#searchResults").html(JSON.stringify(res.data));
+
+    });
+
+    $('#foodSearchButton').click(async () => {
+        if ($('#appleCheckbox').is(':checked')) {
             const query = {
-                type: "weightSearch",
-                minWeight: $('#weightLowerLimitInput').val(),
-                maxWeight: $('#weightUpperLimitInput').val(),
+                type: "foodSearch",
+                loves: "apple",
                 projectionFilters: {
                     name: true,
+                    loves: true,
                     weight: true,
                 }
-            
             }
             const res = await axios.post('https://fantastic-cyan-dress.cyclic.app//search', query)
 
             $("#searchResults").empty();
             $("#searchResults").html(JSON.stringify(res.data));
+        }
+        else if ($('#carrotCheckbox').is(':checked')) {
+            const query = {
+                type: "foodSearch",
+                loves: "carrot",
+                projectionFilters: {
+                    name: true,
+                    loves: true,
+                    weight: true,
+                }
+            }
+            const res = await axios.post('https://fantastic-cyan-dress.cyclic.app//search', query)
 
+            $("#searchResults").empty();
+            $("#searchResults").html(JSON.stringify(res.data));
+        }
+        else {
+            const query = {
+                type: "foodSearch",
+                loves: ["apple", "carrot"],
+                projectionFilters: {
+                    name: true,
+                    loves: true,
+                    weight: true,
+                }
+            }
+            const res = await axios.post('https://fantastic-cyan-dress.cyclic.app//search', query)
+
+            $("#searchResults").empty();
+            $("#searchResults").html(JSON.stringify(res.data));
+        }
     });
 
 
